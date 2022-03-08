2c06a5eb44d2e822d42f450b4294041d5a776110
ChristinaJancy <chri91eq@easv365.dk>
2022-03-08 12:56:48 +0100

    register and login user models/routes

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const schema = mongoose.Schema;
+// userschema for user collection in mongodb   
+let objectSchema = new Schema(
+    {
+        name: { type: String, required: true, min: 6, max: 255 },
+        email: { type: String, required: true, min: 6, max: 255 },
+        password: { type: String, required: true, min: 6, max: 255 },
+        date: { type: Date, default: Date.now }
+    }
+)
 
 
-//Mongoose schema for structuring data
-let objectSchema = new schema({
-    name: {type: String}
-});
+
 
 module.exports = mongoose.model("user", objectSchema);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,94 @@
-const router = require("express").Router();
-const schema = require("../models/users")
-const { verifyToken } = require("../validation")
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const router = require('express').Router();
+const User = require('../models/users');
+const { registerValidation, loginValidation, verifyToken } = require('../validation');
 
-module.exports = router;
 
+
+router.post("/register", async (req, res) => {
+
+    //validate user inputs (name, email, password)
+    const { error } = registerValidation(req.body);
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    //check if email is already registeret
+    const emailExist = await User.findOne({ email: req.body.email });
+
+    if (emailExist) {
+        return res.status(400).json({ error: "Email already exists" });
+    }
+
+    //hash password before saving in database
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+
+    //create user o bject and save it in Mongo (via try-catch)
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password
+    });
+ //     try to save user in database (via try-catch)
+    try { 
+        const savedUser = await user.save(); //save user
+        res.json({ message: "New user created.😊", newUser : savedUser});
+    } catch (error) { //if error, return error
+        res.status(400).json({ error });
+    }
+    // res.send("User registered successfully");
+});
+
+router.post("/login", async (req, res) => {
+
+    //validate user login info (email, password)
+    const { error } = loginValidation(req.body);
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    //if login info is valid find the user by email in database
+    const user = await User.findOne({ email: req.body.email });
+
+    //throw error if email is wrong - user does not exist in DB 
+    if (!user) {
+        return res.status(400).json({ error: "Email is wrong" });
+    }
+
+    //check for password correctness - compare hashed password with password from DB
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    //throw error if password is wrong 
+    if (!validPassword) { 
+        return res.status(400).json({ error: "Password is wrong" })
+    }
+
+    //create authentication token with username and id
+    const token = jwt.sign
+        (
+            //payload data
+            {
+                name: user.name,
+                id: user._id
+            },
+            process.env.TOKEN_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN },
+        );
+
+    res.header("auth-token", token).json({
+        message: "User logged in 😊",
+        data: { token }
+    });
+})
 
 router.get("/", verifyToken, (req, res) => {
-    schema.find()
-        .then(data => { res.send(mapArr(data)); })
+    User.find()
+        .then(data => { res.send(data); })
         .catch(err => { res.status(500).send({ message: err.message }) })
 });
+
+module.exports = router;
