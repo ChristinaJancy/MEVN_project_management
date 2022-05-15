4e0d940b7ebe5c15fa3efea5b60997e755c4bc0f
jona737a <jonathansmadsen@gmail.com>
2022-05-15 15:24:23 +0200

    WIP create project with templates

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -3,7 +3,7 @@ const schema = require("../models/projects")
 const { verifyToken } = require("../validation")
 const column = require("../models/columns")
 const tasks = require("../models/tasks")
-const { deleteColumn } = require("./columns")
+const { deleteColumn } = require("./columns");
 
 module.exports = router;
 
@@ -22,21 +22,53 @@ router.get("/:id", verifyToken, (req, res) => {
 
 // Create new project
 router.post("/", verifyToken, async (req, res) => {
-    const project = new schema({
-        title: req.body.title,
-        deadline: req.body.deadline,
-        description: req.body.description,
-        tags: req.body.tags,
-        columns: req.body.columns,
-        assigned: req.body.assigned
-    })
-    try {
-        const savedProject = await project.save()
-        res.json({ message: "New project created.😊", newproject: savedProject })
+    
+    
+    const assignColumns = async (bodyColumns) => {
+        if (bodyColumns === []) {
+            const columns = [];
+            return columns;
+        } else {
+            // if there are columns in the project create new columns with that name
+            const columns = [];
+            columnNames = bodyColumns;
+            await columnNames.forEach(async columnName => {
+                const columnObject = new column({
+                    title: columnName,
+                    tasks: []
+                })
+                const savedColumn = await columnObject.save()
+                columns.push(savedColumn._id.valueOf())
+                console.log("columns inside forEach", columns)
+            });
+            console.log("columns outside forEach", columns)
+            return columns;
+        };
+    };
 
+
+    try {
+        const projectSchema = await assignColumns(req.body.columns).then(columns => {
+            console.log(columns)
+            const project = new schema({
+                title: req.body.title,
+                deadline: req.body.deadline,
+                description: req.body.description,
+                tags: req.body.tags,
+                columns: columns,
+                assigned: req.body.assigned
+            })
+            return project
+        });
+        
+        console.log(projectSchema)
+        const savedProject = await projectSchema.save()
+        res.json({ message: "New project created.😊", newproject: savedProject })
     } catch (error) {
         res.status(400).json({ message: error.message })
     }
+
+
 });
 
 // Update project by id
